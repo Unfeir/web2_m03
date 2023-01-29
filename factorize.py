4dffb597a546d1fb4ef359cb6f68f55d0a4fc520
Anton Holovin <54354764+Unfeir@users.noreply.github.com>
2023-01-29 15:33:27 +0200

    ad

--- factorize.py
+++ factorize.py
@@ -2,7 +2,7 @@ from time import time
 from multiprocessing import Pool, Process, cpu_count, Semaphore
 import concurrent.futures
 from random import randint
-
+import asyncio
 
 def factorize(number):
     result = []
@@ -19,6 +19,15 @@ def factorize_m(number: int, semaphore: Semaphore):
 
 def factorize_p(number: int):
     return [x for x in range(1, number + 1) if number % x == 0]
+
+
+async def worker(nums):
+    loop = asyncio.get_running_loop()
+
+    with concurrent.futures.ProcessPoolExecutor(cpu_count()) as executor:
+        futures = [loop.run_in_executor(executor, factorize_p, num) for num in nums]
+        r = await asyncio.gather(*futures)
+        return r
 
 # a, b, c, d  = factorize(128, 255, 99999, 10651060)
 #
@@ -52,13 +61,25 @@ if __name__ == '__main__':
     with Pool(processes=cpu_count()) as pool:
         pool.map(factorize_p, n)
     print(f'pool: {time() - start_pool}')
-# metod 4 POOL_EX
+
+ # metod 3 POOL
+    start_pool_2 = time()
+    with Pool(processes=cpu_count()) as pool:
+        pool.map_async(factorize_p, n)
+        # pool.close()  # перестати виділяти процеси в пулл
+        # pool.join()  # дочекатися закінчення всіх процесів
+    print(f"pool: {time() - start_pool_2}")
+
+    # metod 4 POOL_EX
     start_pool_ex = time()
     with concurrent.futures.ProcessPoolExecutor(cpu_count()) as executor:
         for number in n:
             factorize_p(number)
-    print(f'pool_ex: {time() - start_pool_ex}')
-
+    print(f"pool_ex: {time() - start_pool_ex}")
+    # metod 5 asincio
+    start_pool_as = time()
+    r = asyncio.run(worker(n))
+    print(f"pool_async: {time() - start_pool_as}")
 
 """
 result:
@@ -66,10 +87,13 @@ single: 12.971923351287842
 semaphore: 3.5136280059814453
 pool: 3.094470739364624
 pool_ex: 15.939355850219727
-
-
 """
-
+#
+# single: 21.325724840164185
+# semaphore: 6.019000053405762
+# pool: 4.436388969421387
+# pool_ex: 20.5535409450531
+# pool_async: 4.101235628128052
 
 
 
